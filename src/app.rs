aadc4ff1b425418ff84ca52381f537dd82a8ee4e
Atliac <Atliac@outlook.com>
2026-02-25 00:45:32 +0800

    init project

---
+++ src/app.rs
@@ -0,0 +1,10 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <main class="">
+        <h1 class="text-3xl font-bold underline">"Hello, world!"</h1>
+        </main>
+    }
+}
